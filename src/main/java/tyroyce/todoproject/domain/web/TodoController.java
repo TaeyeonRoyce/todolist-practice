41dc3e53f3f09a7effe5e76da7443b7da163b4a1
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2021-12-06 01:03:43 +0900

    feat : add todoAdd Form and return

--- src/main/java/tyroyce/todoproject/domain/web/TodoController.java
+++ src/main/java/tyroyce/todoproject/domain/web/TodoController.java
@@ -8,9 +8,12 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -33,7 +36,7 @@ public class TodoController {
 	}
 
 	@GetMapping("/addTodo")
-	public String addTodo() {
+	public String printAddTodoView() {
 		return "todolist/addTodo";
 	}
 
@@ -42,6 +45,14 @@ public class TodoController {
 		Optional<Todo> todoOptional = todoRepository.findById(todoId);
 		model.addAttribute("todo", todoOptional.get());
 		return "todolist/todo";
+	}
+
+	@PostMapping("/addTodo")
+	public String addTodo(@RequestParam String todoDetail, RedirectAttributes redirectAttributes) {
+		Todo todo = new Todo(todoDetail);
+		Todo savedTodo = todoRepository.save(todo);
+		redirectAttributes.addAttribute("status", true);
+		return "redirect:/todolist/addTodo";
 	}
 
 	@PostConstruct
